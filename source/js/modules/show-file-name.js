040b0f142195560e418bac31163c586bec17c0b4
Alex <bebko07@gmail.com>
2024-02-27 12:35:26 +0300

    создает gh-pages

--- source/js/modules/show-file-name.js
+++
@@ -1,8 +0,0 @@
-const inputFile = document.querySelector('.form__input-file');
-
-export function showFileName() {
-  inputFile.addEventListener('change', function (e) {
-    const fileName = e.target.value.split('\\').pop();
-    document.getElementById('fileName').textContent = `${fileName} загружен`;
-  });
-}
